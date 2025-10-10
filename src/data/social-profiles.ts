6b2154a796e8876c4f46a9fc881d2c9cb5dc0ce2
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2025-10-10 23:18:20 +0300

    social media icons (from uxwing.com ) [Exclusive collection of Free SVG Icons for commercial projects without attribution]

--- src/data/social-profiles.ts
+++ src/data/social-profiles.ts
@@ -6,76 +6,76 @@ export const socialLinks: SocialLink[] = [
         identifier: "@KeresztesZsolt",
         url: "https://www.youtube.com/@KeresztesZsolt",
         description: "Short, concise, and easy-to-understand self-improvement and STEM* content in Hungarian.",
-        icon: "icons/youtube-color-icon.png"
+        icon: "/assets/icons/contact/youtube-color-icon.png"
     },
     {
         title: "YouTube",
         identifier: "@KeresztesZsolti",
         url: "https://www.youtube.com/@KeresztesZsolti",
         description: "Short, concise, and easy-to-understand self-improvement and STEM* content in English, with occasional videos in Romanian and German.",
-        icon: "icons/youtube-color-icon.png"
+        icon: "/assets/icons/contact/youtube-color-icon.png"
     },
     {
         title: "TikTok",
         identifier: "@kereszteszsolti",
         url: "https://www.tiktok.com/@kereszteszsolti",
         description: "Short, concise, and easy-to-understand self-improvement and STEM* content in Hungarian.",
-        icon: "icons/tiktok-square-color-icon.png"
+        icon: "/assets/icons/contact/tiktok-square-color-icon.png"
     },
     {
         title: "TikTok",
         identifier: "@kereszteszsoltie",
         url: "https://www.tiktok.com/@kereszteszsoltie",
         description: "Short, concise, and easy-to-understand self-improvement and STEM* content in English, sometimes in Romanian and German as well.",
-        icon: "icons/tiktok-square-color-icon.png"
+        icon: "/assets/icons/contact/tiktok-square-color-icon.png"
     },
     {
         title: "LinkedIn",
         identifier: "@kereszteszsolt",
         url: "https://www.linkedin.com/in/kereszteszsolt",
         description: "Professional profileInfo and networking.",
-        icon: "icons/linkedin-app-icon.png"
+        icon: "/assets/icons/contact/linkedin-app-icon.png"
     },
     {
         title: "X (Twitter)",
         identifier: "@KeresztesZsolti",
         url: "https://x.com/KeresztesZsolti",
         description: "Updates, thoughts, and sharing content.",
-        icon: "icons/x-social-media-logo-icon.png"
+        icon: "/assets/icons/contact/x-social-media-logo-icon.png"
     },
     {
         title: "Linktree",
         identifier: "@kereszteszsolt",
         url: "https://linktr.ee/kereszteszsolt",
         description: "All my important links in one place.",
-        icon: "icons/linktree-logo-icon.png"
+        icon: "/assets/icons/contact/linktree-logo-icon.png"
     },
     {
         title: "Buy Me A Coffee",
         identifier: "@kereszteszsolt",
         url: "https://www.buymeacoffee.com/kereszteszsolt",
         description: "If you find my content or shared code useful, you can support me on my Buy Me a Coffee page. Your support is completely optional and comes with no obligation or reward – it’s simply a way to show appreciation for my work.",
-        icon: "icons/buy-me-coffee-icon.png"
+        icon: "/assets/icons/contact/buy-me-coffee-icon.png"
     },
     {
         title: "GitHub",
         identifier: "@kereszteszsolt",
         url: "http://www.github.com/kereszteszsolt",
         description: "My open-source projects and contributions.",
-        icon: "icons/github-mark-white-icon.png"
+        icon: "/assets/icons/contact/github-icon.png"
     },
     {
         title: "Instagram",
         identifier: "@kereszteszsolt_kzs",
         url: "https://www.instagram.com/kereszteszsolt_kzs",
         description: "Mostly nature, plants, birds, and bugs photography, taken by phone",
-        icon: "icons/instagram-icon.png"
+        icon: "/assets/icons/contact/ig-instagram-icon.png"
     },
     {
         title: "Facebook",
         identifier: "@KeresztesZsolt.kzs",
         url: "https://www.facebook.com/KeresztesZsolt.kzs",
         description: "Professional page! Some times i shae updates about my content and projects.",
-        icon: "icons/facebook-f-logo-icon.png"
+        icon: "/assets/icons/contact/facebook-square-icon.png"
     }
 ];
